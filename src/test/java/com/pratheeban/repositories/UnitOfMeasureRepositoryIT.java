aa11dd3735eadf735dcaf7b9e82430f1e79ab5c0
unknown <pratheeban.panch@gmail.com>
2018-12-16 11:46:29 -0500

    Adding Junit Test Classes.

---
+++ src/test/java/com/pratheeban/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,42 @@
+package com.pratheeban.repositories;
+
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.pratheeban.domain.UnitOfMeasure;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+	@Autowired
+	UnitOfMeasureRepository unitOfMeasureRepository;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFindByDescription() {
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+		assertEquals("Teaspoon", uomOptional.get().getDescription());
+	}
+	
+	@Test
+    public void findByDescriptionCup() throws Exception {
+
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+
+}
---
+++ src/test/java/com/pratheeban/services/RecipeServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.pratheeban.services;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.pratheeban.domain.Recipe;
+import com.pratheeban.repositories.RecipeRepository;
+
+public class RecipeServiceImplTest {
+
+	RecipeServiceImpl recipeService;
+
+	@Mock
+	RecipeRepository recipeRepository;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+
+		recipeService = new RecipeServiceImpl(recipeRepository);
+	}
+
+	@Test
+	public void getRecipesTest() throws Exception {
+		Recipe recipe = new Recipe();
+		Set<Recipe> receipesData = new HashSet();
+		receipesData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(receipesData);
+
+		Set<Recipe> recipes = recipeService.getRecipes();
+		
+		assertEquals(recipes.size(), 1);		
+        verify(recipeRepository, times(1)).findAll();
+
+	}
+
+}
