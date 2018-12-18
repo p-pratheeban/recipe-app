39a55080a2bcfbdd6311c76d335a1d42f3d2c7a2
unknown <pratheeban.panch@gmail.com>
2018-12-18 04:19:18 -0500

    Adding Validation

--- src/main/java/com/pratheeban/commands/RecipeCommand.java
+++ src/main/java/com/pratheeban/commands/RecipeCommand.java
@@ -7,6 +7,13 @@ import lombok.Setter;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
+
 import com.pratheeban.domain.Difficulty;
 
 @Getter
@@ -14,12 +21,22 @@ import com.pratheeban.domain.Difficulty;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+    @NotBlank
+	@Size(min = 3, max = 255)
     private String description;
+    @Min(1)
+	@Max(999)
     private Integer prepTime;
+    @Min(1)
+	@Max(999)
     private Integer cookTime;
+    @Min(1)
+	@Max(100)
     private Integer servings;
     private String source;
+    @URL
     private String url;
+	@NotBlank
     private String directions;
     private Byte[] image;
     private Set<IngredientCommand> ingredients = new HashSet<>();
--- src/main/java/com/pratheeban/controllers/RecipeController.java
+++ src/main/java/com/pratheeban/controllers/RecipeController.java
@@ -1,8 +1,11 @@
 package com.pratheeban.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,6 +23,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class RecipeController {
+	private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
+
 	private final RecipeService recipeService;
 
 	public RecipeController(RecipeService recipeService) {
@@ -38,11 +43,20 @@ public class RecipeController {
 	public String newRecipe(Model model) {
 		model.addAttribute("recipe", new RecipeCommand());
 
-		return "recipe/recipeform";
+		return RECIPE_RECIPEFORM_URL;
 	}
 
 	@PostMapping("/recipe")
-	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+	public String saveOrUpdate(@Valid @ModelAttribute RecipeCommand command, BindingResult bindingResult) {
+		log.debug("Saving or updating recipe");
+
+		if (bindingResult.hasErrors()) {
+			bindingResult.getAllErrors().forEach(objectError -> {
+				log.debug(objectError.toString());
+			});
+
+			return RECIPE_RECIPEFORM_URL;
+		}
 		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
 		return "redirect:/recipe/" + savedCommand.getId() + "/show";
@@ -52,7 +66,7 @@ public class RecipeController {
 	public String updateRecipe(@PathVariable String id, Model model) {
 		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
 
-		return "recipe/recipeform";
+		return RECIPE_RECIPEFORM_URL;
 	}
 
 	@GetMapping("/recipe/{id}/delete")
